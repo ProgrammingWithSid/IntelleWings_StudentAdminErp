9a1ff8027a34599b97eb00aa21e9c5b80d5baaec
satender <satenderk8700@gmail.com>
2022-12-11 16:25:32 +0530

    StudentAdminErp

---
+++ StudentAdminErp.API/StudentAdminErp.API/StudentAdminErp.API/DataModels/Address.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace StudentAdminErp.API.DataModels
+{
+    public class Address
+    {
+        internal string PhysicalAddress;
+
+        public Guid Id { get; set; }
+
+        public string Physicaladdress { get; set; }
+        public string PostalAddress { get; set; }
+
+        //Navigation Property
+
+        public Guid StudentId { get; set; }
+    }
+}
---
+++ StudentAdminErp.API/StudentAdminErp.API/StudentAdminErp.API/Profiles/AutoMapperProfiles.cs
@@ -0,0 +1,28 @@
+﻿using AutoMapper;
+using StudentAdminErp.API.DomainModels;
+using DataModels = StudentAdminErp.API.DataModels;
+using StudentAdminErp.API.Profiles.AfterMaps;
+
+namespace StudentAdminErp.API.Profiles
+{
+    public class AutoMapperProfiles : Profile
+    {
+        public AutoMapperProfiles()
+        {
+            CreateMap<DataModels.Student, Student>()
+                .ReverseMap();
+
+            CreateMap<DataModels.Gender, Gender>()
+                .ReverseMap();
+
+            CreateMap<DataModels.Address, Address>()
+                .ReverseMap();
+
+            CreateMap<UpdateStudentRequest, DataModels.Student>()
+                .AfterMap<UpdateStudentRequestAfterMap>();
+
+            CreateMap<AddStudentRequest, DataModels.Student>()
+                .AfterMap<AddStudentRequestAfterMap>();
+        }
+    }
+}
